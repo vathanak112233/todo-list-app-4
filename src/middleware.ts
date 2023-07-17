db53c9694140b859ca26305cff4010bca67f5b19
vathanak <vathanakkongsothybtb35@gmail.com>
2023-07-17 13:37:56 +0700

    Validate token every api called

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyJwt } from "./lib/jwt";
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/task/:path*"]
+    matcher: ["/api/:function*"]
 }
+
+export function middleware(request: NextRequest) {
+    const path = new URL(request.url).pathname;
+    if (path.startsWith("/api/auth/")) {
+        return;
+    }
+
+    if (!isAuthenticated(request)) {
+        return new NextResponse(
+            JSON.stringify({ success: false, message: 'authentication failed' }),
+            { status: 401, headers: { 'content-type': 'application/json' } }
+        )
+    }
+}
+
+function isAuthenticated(request: NextRequest) {
+    const accessToken = request.headers.get("authorization");
+    return accessToken && !verifyJwt(accessToken);
+}
+
